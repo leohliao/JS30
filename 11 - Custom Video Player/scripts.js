f641c4356f93bfe7771075f6150e5730481b79c4
leol <ducleoliao@gmail.com>
2017-11-14 23:07:11 -0800

    get elements

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,6 +1,11 @@
 /* Step 01 - Get Our Elements */
 const player = document.querySelector('.player');
-
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip');
+const ranges = player.querySelectorAll('.player__slider');
 
 /* Step 02 - Build Out Functions */
 
