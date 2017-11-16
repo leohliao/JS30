f4d8d422fa5499e2c0fd0c83b9058bb1e1153af6
leol <ducleoliao@gmail.com>
2017-11-15 16:55:48 -0800

    Finished Day11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -6,13 +6,68 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip');
 const ranges = player.querySelectorAll('.player__slider');
+const fullScreen = player.querySelector('.full-screen');
 
 /* Step 02 - Build Out Functions */
-function togglePlay(){
+function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
+  console.log(video);
+} // end togglePlay
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚" ; // this refers to video
+  toggle.textContent = icon;
+  console.log('update the button');
+} // end updateButton
+
+function skip() {
+  console.log(this.dataset.skip)
+  video.currentTime += parseFloat(this.dataset.skip);
+
+} // end skip
+
+function handleRangeUpdate() {
+  console.log(this.value);
+  console.log(this.name);
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100; 
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration; 
+  video.currentTime = scrubTime;
+}
+
+function toggleFullScreen(e) {
+  console.log('this is fullScreen');
+  player.classList.toggle('fullscreen');
 }
 
 /* Step 03 - Hook up the event listener */
-player.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+fullScreen.addEventListener('click', toggleFullScreen);
+
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true );
+progress.addEventListener('mouseup', () => mousedown = false );
+
+
+
+
+
