017f0518f703a0ab2c60eb442278c0d7c6bedac1
leol <ducleoliao@gmail.com>
2017-11-28 23:38:01 -0800

    Finished Day 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -33,11 +33,17 @@ function paintToCanvas() {
     ctx.drawImage(video, 0, 0, width, height);
     /* Step 09 - create filter function to mess with the photo images
               - get picture out from canvas */
-    const pixels = ctx.getImageData(0, 0, width, height)   
+    /* take the pixels out */
+    let pixels = ctx.getImageData(0, 0, width, height)   
     /* create function to manipulate the images */
-    pixels = redEffect(pixels);      
+    // pixels = redEffects(pixels);   
+    // pixels = rgbSplit(pixels);   
+    // ctx.globalAlpha = 0.1;
+
+    pixels = greenScreen(pixels);
     // console.log(pixels);
-    debugger;
+    // put pixels back
+    ctx.putImageData(pixels, 0, 0);
   }, 16);
 
 }
@@ -62,14 +68,50 @@ function takePhoto(){
 }
 
 /* Step 10 - function to manipulate image effects */
-function redEffects([pixels]){
-  for(let i = 0; i < pixels.length; i+=4){
-    pixels.data[i + 0] = pixels.data[i + 0] + 100; // RED
+function redEffects(pixels){
+  for(let i = 0; i < pixels.data.length; i+=4){
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; // RED
     pixels.data[i + 1] = pixels.data[i + 1] - 50; // GREEN
-    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // BLUE
-    
-    
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // BLUE 
   }
+  return pixels;
+};
+
+/* Step 11 - function to manipulate image effects */
+function rgbSplit(pixels){
+  for(let i = 0; i < pixels.data.length; i+=4){
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // BLUE 
+  }
+  return pixels;
+};
+
+/* Step 12 - function to manipulate image effects */
+function greenScreen(pixels){
+  const levels = {};
+  document.querySelectorAll('.rgb input').forEach(input => {
+    levels[input.name] = input.value; 
+  });
+
+  // console.log(levels);
+  for (let i = 0; i < pixels.data.length; i = i + 4){
+    red = pixels.data[i+0];
+    green = pixels.data[i+1];
+    blue = pixels.data[i+2];
+    alpha = pixels.data[i+3];
+
+    if (red >= levels.rmin
+    && green >= levels.gmin
+    && blue >= levels.bmin
+    && red <= levels.rmax
+    && green <= levels.gmax
+    && blue <= levels.bmax) {
+      // take it out! 
+      pixels.data[i+3]=0;
+    }
+  }
+  return pixels;
 };
 
 getVideo();
