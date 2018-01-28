5fdde3752abf2052e2f771e931709c65c4788320
leol <ducleoliao@gmail.com>
2018-01-27 20:50:50 -0800

    Finished angular 1.5

--- angularjs/01-practice/app.js
+++ angularjs/01-practice/app.js
@@ -1,16 +1,28 @@
 (function(){
+  var gems = [
+    {
+    name: "Dodecahedron",
+    price: 2.95,
+    description: 'Your favorite',
+    canPurchase: false,
+    soldOut: false
+    },
+    {
+      name: "Ruby",
+      price: 1.95,
+      description: 'on Rails',
+      canPurchase: false,
+      soldOut: true
+    },
+
+  ];
   /* Step 01 - define our application name and link it with html  */
   var app = angular.module("store", [ ]); // store = application name,  // [] = dependencies
   /* Step 02 - define our controller */
   app.controller("StoreController", function(){
-    this.product = gem;
+    this.product = gems;
   });
 
-  var gem = {
-    name: "Dodecahedron",
-    price: 2.95,
-    description: 'Your favorite',
-  };
 
 })();
 
